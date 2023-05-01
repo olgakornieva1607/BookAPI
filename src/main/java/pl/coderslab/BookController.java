95e09f9a58848a3e5b3936cefa7f9126e06cb6ed
Olga <olga.kornieva1607@gmail.com>
2023-05-01 22:49:00 +0200

    bookApi

---
+++ src/main/java/pl/coderslab/BookController.java
@@ -0,0 +1,56 @@
+package pl.coderslab;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+    public BookController( BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/helloBook")
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+    }
+
+
+    @GetMapping("")
+    @ResponseBody
+    public  List<Book> getList(){
+        return bookService.getBooks();
+    }
+
+    @PostMapping( "")
+    public void addBook(@RequestBody Book book){
+        bookService.add(book);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable Long id){
+        return bookService.get(id).orElseThrow(() ->{
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "entity not found"
+            );
+        });
+    }
+
+    @DeleteMapping("/{id}")
+     public void removeBook(@PathVariable Long id) {
+            bookService.delete(id);
+    }
+
+
+    @PutMapping("")
+    @ResponseBody
+    public void updateBook(@RequestBody Book book) {
+        bookService.update(book);
+    }
+}
