da2ffaaed4908ee102ea76e089c9f0cb3971341d
Olga <olga.kornieva1607@gmail.com>
2023-05-02 02:18:58 +0200

    bookApi

--- src/main/java/pl/coderslab/Book.java
+++ src/main/java/pl/coderslab/Book.java
@@ -1,12 +1,27 @@
 package pl.coderslab;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "books")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String isbn;
     private String title;
     private String author;
     private String publisher;
     private String type;
+
 
     public Book(Long id, String isbn, String title, String author, String publisher, String type) {
         this.id = id;
@@ -17,55 +32,8 @@ public class Book {
         this.type = type;
     }
 
-    public Book(){}
 
+    public Book() {
 
-    public Long getId() {
-        return id;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
 }
--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,11 +1,12 @@
 package pl.coderslab;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/books")
 public class BookController {
